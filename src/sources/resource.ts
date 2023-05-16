852eeb64a71529d70851a540f6dd38ca4f4690d1
Tevon <tevon@bruinen.co>
2023-05-16 13:44:22 -0700

    new build

--- src/sources/resource.ts
+++ src/sources/resource.ts
@@ -9,8 +9,8 @@ export class Resource<Params, ReturnType> {
   public type: string;
   public description: string;
   public action: Action<Params, ReturnType>;
-  public JSONInputSchema: any;
-  public JSONOutputSchema: any;
+  public JSONInputSchema: unknown;
+  public JSONOutputSchema: unknown;
   public constructor(
     name: string,
     friendlyName: string,
@@ -18,7 +18,7 @@ export class Resource<Params, ReturnType> {
     description: string,
     action: Action<Params, ReturnType>,
     JSONInputSchema: unknown,
-    JSONOutputSchema: unknown,
+    JSONOutputSchema: unknown
   ) {
     this.name = name;
     this.friendlyName = friendlyName;
