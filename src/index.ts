e4ba68560b8eb8c6da4899f65be92e543120b10b
Grant Pierce <glpierce174@gmail.com>
2023-06-12 16:20:07 -0700

    Added slack in index files

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,12 @@
 import { Github } from "./sources/github/github";
 import { Google } from "./sources/google/google";
+import { Slack } from "./sources/slack/slack";
 
 const Sources: { [x: string]: any } = {};
 Sources[new Github().getName()] = Github;
 Sources[new Google().getName()] = Google;
+Sources[new Slack().getName()] = Slack;
 
 export default Sources;
 
-export { Github, Google };
+export { Github, Google, Slack };
--- src/sources/index.ts
+++ src/sources/index.ts
@@ -1,12 +1,15 @@
 import { Github } from "./github/github";
 import { Google } from "./google/google";
+import { Slack } from "./slack/slack";
 import { Source } from "./source";
 
 const githubName = new Github().getName();
 const googleName = new Google().getName();
+const slackName = new Slack().getName();
 
 const sources: { [x: string]: any } = {};
 sources[githubName] = Github;
 sources[googleName] = Google;
+sources[slackName] = Slack;
 
 export default sources;
